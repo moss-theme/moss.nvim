d44dc9d47e88ac663e5d5182e0eb038d90e171b6
Alchemmist <anton.ingrish@gmail.com>
2025-07-31 18:52:04 +0300

    fix syntax for go

---
+++ after/queries/go/highlights.scm
@@ -0,0 +1,1 @@
+(channel_type) @keyword.chan
--- lua/nothing.lua
+++ lua/nothing.lua
@@ -178,6 +178,7 @@ M.hl_langs = function()
 
 	-- Go specific
 	hl(0, "@property.go", { fg = colors.gray })
+    hl(0, "@keyword.chan", { fg = colors.keywords_and_operators })
 
 	-- Dart specific
 	hl(0, "@property.dart", { fg = colors.gray })
