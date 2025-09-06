b701eb461d9dbe3ec65ba404bf384bae8274af2d
Alchemmist <anton.ingrish@gmail.com>
2025-09-06 15:58:17 +0300

    try pastel

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,16 +1,16 @@
 return {
-  constant = "#8FB3C2",    -- Приглушенный голубой
-  function_definitions = "#A6B4C6", -- Серо-голубой
-  keywords_and_operators = "#7C9EB7", -- Пыльный синий
-  type_definitions = "#6C8DB5",    -- Более насыщенный, но все же холодный синий
-  comment = "#65727B",    -- Холодный серо-синий
-  string = "#82A6AC",     -- Мятно-серый
+  constant = "#EBCB8B",    -- Мягкий песочный
+  function_definitions = "#B48EAD", -- Приглушенная сирень
+  keywords_and_operators = "#A3BE8C", -- Мягкий салатовый
+  type_definitions = "#81A1C1",    -- Нежный голубой
+  comment = "#9196A1",    -- Светлый серый
+  string = "#D08770",     -- Мягкий коралловый
 
-  black = "#0F1418",      -- Глубокий синевато-черный
-  gray = "#8C9AA6",       -- Холодный серый
-  lgray = "#B5C0CC",      -- Светлый холодный серый
-  indent = "#141A20",     -- Темный сине-серый
-  visual = "#1A2330",     -- Синеватое выделение
+  black = "#2E3440",      -- Темный серо-синий (как в Nord)
+  gray = "#9196A1",       -- Средний серый
+  lgray = "#D8DEE9",      -- Светлый серый
+  indent = "#3B4252",     -- Чуть светлее black
+  visual = "#434C5E",     -- Средний серо-синий
 
   transparent = "NONE",
 }
