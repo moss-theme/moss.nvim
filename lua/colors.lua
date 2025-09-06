5023817c0e498043a619f0d6dcd820142a6d9f0e
Alchemmist <anton.ingrish@gmail.com>
2025-09-06 15:51:49 +0300

    more light colors

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,16 +1,16 @@
 return {
-	constant = "#E6B450",
-	function_definitions = "#FFB454",
-	keywords_and_operators = "#FF8F40",
-	type_definitions = "#59C2FF",
-	comment = "#626A73",
-	string = "#C2D94C",
+  constant = "#8C8C8C",    -- Средний серый
+  function_definitions = "#A6A6A6", -- Светло-серый
+  keywords_and_operators = "#737373", -- Темно-серый
+  type_definitions = "#999999",    -- Еще один оттенок серого
+  comment = "#5C5C5C",    -- Темный серый
+  string = "#8C8C8C",     -- Идентично constant, чтобы не выделяться
 
-	black = "#0A0E14",
-	gray = "#B3B1AD",
-	lgray = "#B9B9B9",
-	indent = "#181A1D",
-	visual = "#1C2631",
+  black = "#0A0A0A",      -- Почти черный
+  gray = "#8C8C8C",       -- Основной серый
+  lgray = "#BFBFBF",      -- Светлый серый для текста
+  indent = "#111111",     -- Еле заметная разница с black
+  visual = "#1F1F1F",     -- Очень темный серый для выделения
 
-	transparent = "NONE",
+  transparent = "NONE",
 }
